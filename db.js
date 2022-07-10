5086dad10cce01a8fd20cbf66ee9e7dd2f5d0b49
Saket Mundra <saketmundra2707@gmail.com>
2022-07-11 01:51:05 +0530

    hwllo

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb+srv://saketmundra:saketsuyash@cluster0.007sqeu.mongodb.net/test"
+const mongoURI="mongodb+srv://saketmundra:saketsuyash@cluster0.007sqeu.mongodb.net/inotebook"
 
 const connecttoMongo=()=>{
     mongoose.connect(mongoURI,()=>{
